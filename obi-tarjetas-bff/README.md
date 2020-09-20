9cbcbe30d8eb5f009c6a2418ac39a46b1c1d162b
Santi De Simone <sd26688@gscorp.ad>
2020-09-20 19:27:12 -0300

    init

--- obi-tarjetas-bff/README.md
+++
@@ -1,1 +0,0 @@
-// Todo: write meaningful README.md
--- run.js
+++ run.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const fs = require('fs-extra');
+const fse = require('fs-extra');
+const fs = require('fs');
 
 // node example.js create --name cards // node example.js create --name=cards-ms // node example.js create cards-ms
-
 require('yargs')
   .scriptName("project-creation")
   .usage('$0 <cmd> [args]')
@@ -16,22 +16,14 @@ require('yargs')
         })
   }, function (argv) {
 
-    // TO DO: CLONE REPO IN CURRENT DIRECTORY 
-    // TO DO: COLOCAR COMO DEPENDENCIA A SPV-BASE 
-    const template_path = require("spv-base");
-    let projectName = argv.name;
-    // // set projectName
-    // // set destinationDirectory
-
-    console.log(template_path.dirname)
-    fs.copySync(template_path.dirname, "./"+projectName);
-    // console.log('your project', argv.name, 'was created!')
-
+      const template_path = require("spv-base");
+      let projectName = argv.name;
+      fse.copySync(template_path.dirname, "./"+projectName);
+      console.log('your project', argv.name, 'was created!')
 
   })
   .help()
   .argv
-
 
 // node run.js add accesspoint -n modulename -v GET POST PUT DELETE -r routename cards
 // node run.js add accesspoint --name modulename --verbs GET POST PUT DELETE --route routename cards
@@ -61,19 +53,66 @@ require('yargs')
     .demand('r') // fail if 'q' not provided.
     .alias('route', 'r')
 
-
   }, function (argv) {
 
-    console.log()
-    console.log()
-    console.log(argv.name)
-    console.log()
-    console.log(argv.verbs)
-    console.log()
-    console.log(argv.route)
-    console.log()
-    console.log()
+      const template_path = require("spv-extension-rest").paths;
+      let accessPointName = argv.name;
+      let accessPointVerbsArray = argv.verbs;
+      let accessPointNameRoute = argv.route;
+      let destinationDirectory;
 
+      // validate we are in the root of a spv project 
+      if ( !fs.existsSync(process.cwd()+'/spv.json') ) {
+        return console.log("current directory is not a supervielle project")
+      };
+
+      // validate we find modules folder
+      if ( !fs.existsSync(process.cwd()+'/src/modules') ) {
+        return console.log("the installer found no 'src/modules' folder")
+      }
+      // set destinationDirectory
+      destinationDirectory = process.cwd()+'/src/modules/'+accessPointName
+
+      fse.copySync(template_path.templates, destinationDirectory);
+
+        const path = require('path');
+        const util = require('util');
+        
+        const traverse = function(dir, result = []) {
+        
+            // list files in directory and loop through
+            fs.readdirSync(dir).forEach((file) => {
+        
+                // builds full path of file
+                const fPath = path.resolve(dir, file);
+        
+                // prepare stats obj
+                const fileStats = { file, path: fPath };
+        
+                // is the file a directory ? 
+                // if yes, traverse it also, if no just add it to the result
+                if (fs.statSync(fPath).isDirectory()) {
+                    fileStats.type = 'dir';
+                    fileStats.files = [];
+                    result.push(fileStats);
+                    return traverse(fPath, fileStats.files)
+                }
+        
+                fileStats.type = 'file';
+
+                  let requiredFile = require(fPath)
+                  let templateFile = requiredFile;
+
+                  templateFile = requiredFile.replace(new RegExp("{className}", 'g'), accessPointName.charAt(0).toUpperCase() + accessPointName.slice(1) );
+                  templateFile = templateFile.replace(new RegExp("{route}", 'g'), '/"'+accessPointNameRoute+'"');
+                  templateFile = templateFile.replace(new RegExp("{verbs}", 'g'), JSON.stringify(accessPointVerbsArray) );
+
+                result.push(fileStats);
+            });
+            return result;
+        };
+
+        console.log(traverse(destinationDirectory))
 
   })
   .help()
